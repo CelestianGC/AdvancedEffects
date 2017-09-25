f009be5a157a0b8db71addc9de6a8d69ddcb6cd0
MikeW <uce_mike@yahoo.com>
2017-09-25 15:00:47 -0500

    item/effects
    * added multi-effect on item.

--- scripts/manager_effect_adnd.lua
+++ scripts/manager_effect_adnd.lua
@@ -1,22 +1,22 @@
-
 --
 -- Effects on Items, apply to character in CT
 --
 --
 -- add the effect if the item is equipped and doesn't exist already
 function updateItemEffects(nodeItem)
+--Debug.console("manager_effect_adnd.lua","updateItemEffects1","nodeItem",nodeItem);
     local nodeChar = DB.getChild(nodeItem, "...");
     if not nodeChar then
         return;
     end
     local sUser = User.getUsername();
     local sName = DB.getValue(nodeItem, "name", "");
-    local sLabel = DB.getValue(nodeItem, "effect", "");
     -- we swap the node to the combat tracker node
     -- so the "effect" is written to the right node
     if not string.match(nodeChar.getPath(),"^combattracker") then
         nodeChar = getCTNodeByNodeChar(nodeChar);
     end
+--Debug.console("manager_effect_adnd.lua","updateItemEffects","nodeChar3",nodeChar);
     -- if not in the combat tracker bail
     if not nodeChar then
         return;
@@ -24,25 +24,43 @@ function updateItemEffects(nodeItem)
     
     local nCarried = DB.getValue(nodeItem, "carried", 0);
     local bEquipped = (nCarried == 2);
-    local sItemSource = nodeItem.getPath();
     local nIdentified = DB.getValue(nodeItem, "isidentified", 0);
+    local bOptionID = OptionsManager.isOption("MIID", "on");
+    if not bOptionID then 
+        nIdentified = 1;
+    end
+
+-- Debug.console("manager_effect_adnd.lua","updateItemEffects","sUser",sUser);
+-- Debug.console("manager_effect_adnd.lua","updateItemEffects","nodeChar",nodeChar);
+-- Debug.console("manager_effect_adnd.lua","updateItemEffects","nodeItem",nodeItem);
+-- Debug.console("manager_effect_adnd.lua","updateItemEffects","nCarried",nCarried);
+-- Debug.console("manager_effect_adnd.lua","updateItemEffects","bEquipped",bEquipped);
+-- Debug.console("manager_effect_adnd.lua","updateItemEffects","nIdentified",nIdentified);
+
+    for _,nodeItemEffect in pairs(DB.getChildren(nodeItem, "effectlist")) do
+        updateItemEffect(nodeItemEffect, sName, nodeChar, sUser, bEquipped, nIdentified);
+    end -- for item's effects list
+end
+
+-- update single effect for item
+function updateItemEffect(nodeItemEffect, sName, nodeChar, sUser, bEquipped, nIdentified)
     local sCharacterName = DB.getValue(nodeChar, "name", "");
- -- Debug.console("manager_effect_adnd.lua","updateItemEffects","nodeChar",nodeChar);
- -- Debug.console("manager_effect_adnd.lua","updateItemEffects","nodeItem",nodeItem);
- -- Debug.console("manager_effect_adnd.lua","updateItemEffects","sName",sName);
- -- Debug.console("manager_effect_adnd.lua","updateItemEffects","sLabel",sLabel);
- -- Debug.console("manager_effect_adnd.lua","updateItemEffects","nCarried",nCarried);
- -- Debug.console("manager_effect_adnd.lua","updateItemEffects","bEquipped",bEquipped);
- -- Debug.console("manager_effect_adnd.lua","updateItemEffects","sItemSource",sItemSource);
- -- Debug.console("manager_effect_adnd.lua","updateItemEffects","nIdentified",nIdentified);
+    local sItemSource = nodeItemEffect.getPath();
+    local sLabel = DB.getValue(nodeItemEffect, "effect", "");
+-- Debug.console("manager_effect_adnd.lua","updateItemEffect","sName",sName);
+-- Debug.console("manager_effect_adnd.lua","updateItemEffect","sLabel",sLabel);
+-- Debug.console("manager_effect_adnd.lua","updateItemEffect","sItemSource",sItemSource);
     if sLabel and sLabel ~= "" then -- if we have effect string
         local bFound = false;
         for _,nodeEffect in pairs(DB.getChildren(nodeChar, "effects")) do
             local nActive = DB.getValue(nodeEffect, "isactive", 0);
+--Debug.console("manager_effect.lua","updateItemEffect","nActive",nActive);
             if (nActive ~= 0) then
                 local sEffSource = DB.getValue(nodeEffect, "source_name", "");
+--Debug.console("manager_effect.lua","updateItemEffect","sEffSource",sEffSource);
                 if (sEffSource == sItemSource) then
                     bFound = true;
+--Debug.console("manager_effect.lua","updateItemEffect","bFound!!!",bFound);
                     if (not bEquipped) then
                         -- BUILD MESSAGE
                         local msg = {font = "msgfont", icon = "roll_effect"};
@@ -67,6 +85,7 @@ function updateItemEffects(nodeItem)
                             end
                         end
                     
+--Debug.console("manager_effect_adnd.lua","updateItemEffect","!!!bEquipped",bEquipped);
                         nodeEffect.delete();
                         break;
                     end -- not equipped
@@ -74,21 +93,42 @@ function updateItemEffects(nodeItem)
             end -- was active
         end -- nodeEffect for
         
+--Debug.console("manager_effect_adnd.lua","updateItemEffect","pre bEquipped",bEquipped);
         if (not bFound and bEquipped) then
+--Debug.console("manager_effect_adnd.lua","updateItemEffect","bFound and bEquipped",bEquipped);
             local rEffect = {};
+            local nRollDuration = 0;
+            local dDurationDice = DB.getValue(nodeItemEffect, "durdice");
+            local nModDice = DB.getValue(nodeItemEffect, "durmod", 0);
+            if (dDurationDice and dDurationDice ~= "") then
+                nRollDuration = StringManager.evalDice(dDurationDice, nModDice);
+            else
+                nRollDuration = nModDice;
+            end
+            local nDMOnly = 0;
+            local sVisibility = DB.getValue(nodeItemEffect, "visibility", "");
+            if sVisibility == "show" then
+                nDMOnly = 0;
+            elseif sVisibility == "hide" then
+                nDMOnly = 1;
+            elseif nIdentified > 0 then
+                nDMOnly = 0;
+            end
+            
+            rEffect.nDuration = nRollDuration;
             rEffect.sName = sName .. ";" .. sLabel;
             rEffect.sLabel = sLabel; 
-            rEffect.nDuration = 0;
-            rEffect.sUnits = "day";
+            rEffect.sUnits = DB.getValue(nodeItemEffect, "durunit", "day");
             rEffect.nInit = 0;
             rEffect.sSource = sItemSource;
-            rEffect.nGMOnly = nIdentified;
-            rEffect.sApply = "";        
+            rEffect.nGMOnly = nDMOnly;
+            rEffect.sApply = "";
+--Debug.console("manager_effect_adnd.lua","updateItemEffect","rEffect",rEffect);
             EffectManager.addEffect(sUser, "", nodeChar, rEffect, true);
         end
-        
     end
 end
+
 
 -- return the CTnode by using character sheet node 
 function getCTNodeByNodeChar(nodeChar)
