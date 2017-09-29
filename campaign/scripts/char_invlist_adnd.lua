269957cd9434fecaafaae114a246d2f8baca552b
MikeW <uce_mike@yahoo.com>
2017-09-29 17:56:02 -0500

    item
    * effects edited will immediately update on character if in CT.
    * rev update 1.3

--- campaign/scripts/char_invlist_adnd.lua
+++ campaign/scripts/char_invlist_adnd.lua
@@ -26,7 +26,12 @@ function onInit()
 	DB.addHandler(DB.getPath(node, "*.carried"), "onUpdate", onCarriedChanged);
 	DB.addHandler(DB.getPath(node, "*.weight"), "onUpdate", onEncumbranceChanged);
 	DB.addHandler(DB.getPath(node, "*.count"), "onUpdate", onEncumbranceChanged);
-	DB.addHandler(DB.getPath(node, "*.effect"), "onUpdate", updateItemEffectsForEdit);
+	DB.addHandler(DB.getPath(node, "*.effectlist.*.effect"), "onUpdate", updateItemEffectsForEdit);
+	DB.addHandler(DB.getPath(node, "*.effectlist.*.durdice"), "onUpdate", updateItemEffectsForEdit);
+	DB.addHandler(DB.getPath(node, "*.effectlist.*.durmod"), "onUpdate", updateItemEffectsForEdit);
+	DB.addHandler(DB.getPath(node, "*.effectlist.*.name"), "onUpdate", updateItemEffectsForEdit);
+	DB.addHandler(DB.getPath(node, "*.effectlist.*.durunit"), "onUpdate", updateItemEffectsForEdit);
+	DB.addHandler(DB.getPath(node, "*.effectlist.*.visibility"), "onUpdate", updateItemEffectsForEdit);
 	DB.addHandler(DB.getPath(node), "onChildDeleted", updateFromDeletedInventory);
 end
 
@@ -43,7 +48,12 @@ function onClose()
 	DB.removeHandler(DB.getPath(node, "*.carried"), "onUpdate", onCarriedChanged);
 	DB.removeHandler(DB.getPath(node, "*.weight"), "onUpdate", onEncumbranceChanged);
 	DB.removeHandler(DB.getPath(node, "*.count"), "onUpdate", onEncumbranceChanged);
-	DB.removeHandler(DB.getPath(node, "*.effect"), "onUpdate", updateItemEffectsForEdit);
+	DB.removeHandler(DB.getPath(node, "*.effectlist.*.effect"), "onUpdate", updateItemEffectsForEdit);
+	DB.removeHandler(DB.getPath(node, "*.effectlist.*.durdice"), "onUpdate", updateItemEffectsForEdit);
+	DB.removeHandler(DB.getPath(node, "*.effectlist.*.durmod"), "onUpdate", updateItemEffectsForEdit);
+	DB.removeHandler(DB.getPath(node, "*.effectlist.*.name"), "onUpdate", updateItemEffectsForEdit);
+	DB.removeHandler(DB.getPath(node, "*.effectlist.*.durunit"), "onUpdate", updateItemEffectsForEdit);
+	DB.removeHandler(DB.getPath(node, "*.effectlist.*.visibility"), "onUpdate", updateItemEffectsForEdit);
 	DB.removeHandler(DB.getPath(node), "onChildDeleted", updateFromDeletedInventory);
 end
 
@@ -224,17 +234,21 @@ end
 
 -- find the effect for this source and delete and re-build
 function checkEffectsAfterEdit(itemNode)
-    local nodeChar = DB.getChild(itemNode, "...");
-    local nodeCT = EffectManagerADND.getCTNodeByNodeChar(nodeChar);
+    local nodeChar = DB.getChild(itemNode, ".....");
+--Debug.console("char_invlist.lua","checkEffectsAfterEdit","nodeChar",nodeChar);
+--Debug.console("char_invlist.lua","checkEffectsAfterEdit","itemNode",itemNode);
+    local nodeCT = CharManager.getCTNodeByNodeChar(nodeChar);
     if nodeCT then
         for _,nodeEffect in pairs(DB.getChildren(nodeCT, "effects")) do
             local sLabel = DB.getValue(nodeEffect, "label", "");
             local sEffSource = DB.getValue(nodeEffect, "source_name", "");
             -- see if the node exists and if it's in an inventory node
             local nodeFound = DB.findNode(sEffSource);
+--Debug.console("char_invlist.lua","checkEffectsAfterEdit","sEffSource",sEffSource);
+--Debug.console("char_invlist.lua","checkEffectsAfterEdit","nodeFound",nodeFound);
             if nodeFound and nodeFound == itemNode and string.match(sEffSource,"inventorylist") then
                 nodeEffect.delete();
-                EffectManagerADND.updateItemEffects(itemNode);
+                EffectManagerADND.updateItemEffects(DB.getChild(itemNode, "..."));
             end
         end
     end
