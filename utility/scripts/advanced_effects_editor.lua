b774840f9f650700458d2ceb0fc3b54c1d6ffb57
MikeW <uce_mike@yahoo.com>
2017-11-06 11:09:11 -0600

    npcs
    * set effects on npcs to default to "hide" cause, well, it makes sense.

--- utility/scripts/advanced_effects_editor.lua
+++ utility/scripts/advanced_effects_editor.lua
@@ -5,7 +5,16 @@
 
 function onInit()
     local node = getDatabaseNode();
-    DB.getValue(node,"save_type","modifier");
+    --DB.getValue(node,"save_type","modifier");
+    -- if npc and no effect yet then we set the 
+    -- visibility default to hidden
+    if (node.getPath():match("^npc%.id%-%d+")) then
+        local sVisibility = DB.getValue(node,"visibility");
+        local sEffectString = DB.getValue(node,"effect");
+        if (sVisibility == "" and sEffectString == "") then
+            DB.setValue(node,"visibility","string","hide");
+        end
+    end
     DB.addHandler(DB.getPath(node, ".type"),"onUpdate", update);
     DB.addHandler(DB.getPath(node, ".save_type"), "onUpdate", updateSaveEffects);
     DB.addHandler(DB.getPath(node, ".save"), "onUpdate", updateSaveEffects);
