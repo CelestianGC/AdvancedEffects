7af46313dc864480e1a482be0b2af99967529dc6
Mike Wilson <uce.mikew@gmail.com>
2020-12-09 11:16:32 -0600

    4.7, support for versatile weapons
    4.7, support for versatile weapons

--- campaign/scripts/char_weapon_adnd.lua
+++ campaign/scripts/char_weapon_adnd.lua
@@ -4,16 +4,21 @@
 --
 
 function onInit()
-	local node = getDatabaseNode();
-	DB.addHandler(node.getNodeName(), "onChildUpdate", onDataChanged);
-	DB.addHandler(DB.getPath(DB.getChild(node, "..."), "abilities.*.score"), "onUpdate", onDataChanged);
+	local nodeWeapon = getDatabaseNode();
+	local nodeChar = DB.getChild(nodeWeapon, "...");
+	DB.addHandler(nodeWeapon.getNodeName(), "onChildUpdate", onDataChanged);
+	DB.addHandler(DB.getPath(nodeChar, "abilities.*.score"), "onUpdate", onDataChanged);
+	DB.addHandler(DB.getPath(nodeChar, "weapon.twoweaponfighting"), "onUpdate", onDataChanged);
+
 	onDataChanged();
 end
 
 function onClose()
-	local node = getDatabaseNode();
-	DB.removeHandler(node.getNodeName(), "onChildUpdate", onDataChanged);
-	DB.removeHandler(DB.getPath(DB.getChild(node, "..."), "abilities.*.score"), "onUpdate", onDataChanged);
+	local nodeWeapon = getDatabaseNode();
+	local nodeChar = DB.getChild(nodeWeapon, "...");
+	DB.removeHandler(nodeWeapon.getNodeName(), "onChildUpdate", onDataChanged);
+	DB.removeHandler(DB.getPath(nodeChar, "abilities.*.score"), "onUpdate", onDataChanged);
+	DB.removeHandler(DB.getPath(nodeChar, "weapon.twoweaponfighting"), "onUpdate", onDataChanged);
 end
 
 local m_sClass = "";
@@ -50,74 +55,34 @@ function highlightAttack(bOnControl)
 		attackshade.setFrame(nil);
 	end
 end
-			
+	
+function onAttackChanged()
+	local nodeWeapon = getDatabaseNode();
+	local nodeChar = nodeWeapon.getChild("...")
+
+	local nMod = CharWeaponManager.getAttackBonus(nodeChar, nodeWeapon);
+	
+	attackview.setValue(nMod);
+end
+
 function onAttackAction(draginfo)
 	local nodeWeapon = getDatabaseNode();
 	local nodeChar = nodeWeapon.getChild("...")
-	local rActor = ActorManager.getActor("pc", nodeChar);
-	local rAction = {};
-    -- add itemPath to rActor so that when effects are checked we can 
+
+	-- Build basic attack action record
+	local rAction = CharWeaponManager.buildAttackAction(nodeChar, nodeWeapon);
+
+	-- add itemPath to rActor so that when effects are checked we can 
     -- make compare against action only effects
     local _, sRecord = DB.getValue(nodeWeapon, "shortcut", "", "");
 	rActor.itemPath = sRecord;
---Debug.console("char_weapon_adnd.lua","onAttackAction","sRecord",sRecord);    
-    --
-	
-	local aWeaponProps = StringManager.split(DB.getValue(nodeWeapon, "properties", ""):lower(), ",", true);
-	
-	rAction.label = name.getValue();
-	rAction.stat = DB.getValue(nodeWeapon, "attackstat", "");
-	if type.getValue() == 2 then
-		rAction.range = "R";
-		if rAction.stat == "" then
-			rAction.stat = "strength";
-		end
-	elseif type.getValue() == 1 then
-		rAction.range = "R";
-		if rAction.stat == "" then
-			rAction.stat = "dexterity";
-		end
-	else
-		rAction.range = "M";
-		if rAction.stat == "" then
-			rAction.stat = "strength";
-		end
-	end
-	rAction.modifier = DB.getValue(nodeWeapon, "attackbonus", 0) + ActorManager2.getAbilityBonus(rActor, rAction.stat);
-	
-	if prof.getValue() == 1 then
-		rAction.modifier = rAction.modifier + DB.getValue(nodeChar, "profbonus", 0);
-	end
-	rAction.bWeapon = true;
+	-- end Adanced Effects Piece ---
 	
 	-- Decrement ammo
-	local nMaxAmmo = DB.getValue(nodeWeapon, "maxammo", 0);
-	if nMaxAmmo > 0 then
-		local nUsedAmmo = DB.getValue(nodeWeapon, "ammo", 0);
-		if nUsedAmmo >= nMaxAmmo then
-			ChatManager.Message(Interface.getString("char_message_atkwithnoammo"), true, rActor);
-		else
-			DB.setValue(nodeWeapon, "ammo", "number", nUsedAmmo + 1);
-		end
-	end
+	CharWeaponManager.decrementAmmo(nodeChar, nodeWeapon);
 	
-	-- Determine crit range
-	local nCritThreshold = 20;
-	if rAction.range == "R" then
-		nCritThreshold = DB.getValue(nodeChar, "weapon.critrange.ranged", 20);
-	else
-		nCritThreshold = DB.getValue(nodeChar, "weapon.critrange.melee", 20);
-	end
-	for _,vProperty in ipairs(aWeaponProps) do
-		local nPropCritRange = tonumber(vProperty:match("crit range (%d+)")) or 20;
-		if nPropCritRange < nCritThreshold then
-			nCritThreshold = nPropCritRange;
-		end
-	end
-	if nCritThreshold > 1 and nCritThreshold < 20 then
-		rAction.nCritRange = nCritThreshold;
-	end
-	
+	-- Perform action
+	local rActor = ActorManager.getActor("pc", nodeChar);
 	ActionAttack.performRoll(draginfo, rActor, rAction);
 	return true;
 end
@@ -125,127 +90,33 @@ end
 function onDamageAction(draginfo)
 	local nodeWeapon = getDatabaseNode();
 	local nodeChar = nodeWeapon.getChild("...")
-	local rActor = ActorManager.getActor("pc", nodeChar);
-    -- add itemPath to rActor so that when effects are checked we can 
+
+	-- Build basic damage action record
+	local rAction = CharWeaponManager.buildDamageAction(nodeChar, nodeWeapon);
+
+	-- add itemPath to rActor so that when effects are checked we can 
     -- make compare against action only effects
     local _, sRecord = DB.getValue(nodeWeapon, "shortcut", "", "");
 	rActor.itemPath = sRecord;
-    --
-
-	local aWeaponProps = StringManager.split(DB.getValue(nodeWeapon, "properties", ""):lower(), ",", true);
+	-- end Adanced Effects Piece ---
 	
-	local rAction = {};
-	rAction.bWeapon = true;
-	rAction.label = DB.getValue(nodeWeapon, "name", "");
-	if type.getValue() == 0 then
-		rAction.range = "M";
-	else
-		rAction.range = "R";
-	end
-
-	local sBaseAbility = "strength";
-	if type.getValue() == 1 then
-		sBaseAbility = "dexterity";
-	end
-	
-	-- Check for reroll tag
-	for _,vProperty in ipairs(aWeaponProps) do
-		local nPropReroll = tonumber(vProperty:match("reroll (%d+)")) or 0;
-		if nPropReroll > 0 then
-			rAction.nReroll = nPropReroll;
-		end
-	end
-	
-	rAction.clauses = {};
-	local aDamageNodes = UtilityManager.getSortedTable(DB.getChildren(nodeWeapon, "damagelist"));
-	for _,v in ipairs(aDamageNodes) do
-		local sDmgAbility = DB.getValue(v, "stat", "");
-		if sDmgAbility == "base" then
-			sDmgAbility = sBaseAbility;
-		end
-		local nAbilityBonus = ActorManager2.getAbilityBonus(rActor, sDmgAbility);
-		local nMult = DB.getValue(v, "statmult", 1);
-		if nAbilityBonus > 0 and nMult ~= 1 then
-			nAbilityBonus = math.floor(nMult * nAbilityBonus);
-		end
-		local aDmgDice = DB.getValue(v, "dice", {});
-		local aDmgReroll = nil;
-		if rAction.nReroll then
-			aDmgReroll = {};
-			for kDie,vDie in ipairs(aDmgDice) do
-				aDmgReroll[kDie] = rAction.nReroll;
-			end
-		end
-		local nDmgMod = nAbilityBonus + DB.getValue(v, "bonus", 0);
-		local sDmgType = DB.getValue(v, "type", "");
-		
-		table.insert(rAction.clauses, { dice = aDmgDice, stat = sDmgAbility, statmult = nMult, modifier = nDmgMod, dmgtype = sDmgType, reroll = aDmgReroll });
-	end
-	
+	-- Perform damage action
+	local rActor = ActorManager.getActor("pc", nodeChar);
 	ActionDamage.performRoll(draginfo, rActor, rAction);
 	return true;
+
 end
 
-function onAttackChanged()
+function onDamageAction(draginfo)
 	local nodeWeapon = getDatabaseNode();
 	local nodeChar = nodeWeapon.getChild("...")
+
+	-- Build basic damage action record
+	local rAction = CharWeaponManager.buildDamageAction(nodeChar, nodeWeapon);
+	
+	-- Perform damage action
 	local rActor = ActorManager.getActor("pc", nodeChar);
-
-	local sAbility = DB.getValue(nodeWeapon, "attackstat", "");
-	if sAbility == "" then
-		if type.getValue() == 1 then
-			sAbility = "dexterity";
-		else
-			sAbility = "strength";
-		end
-	end
-	local nMod = DB.getValue(nodeWeapon, "attackbonus", 0) + ActorManager2.getAbilityBonus(rActor, sAbility);
-
-	if prof.getValue() ~= 0 then
-		nMod = nMod + DB.getValue(nodeChar, "profbonus", 0);
-	end
-	
-	attackview.setValue(nMod);
-end
-
-function onDamageChanged()
-	local nodeWeapon = getDatabaseNode();
-	local nodeChar = nodeWeapon.getChild("...")
-	local rActor = ActorManager.getActor("pc", nodeChar);
-	
-	local sBaseAbility = "strength";
-	if type.getValue() == 1 then
-		sBaseAbility = "dexterity";
-	end
-	
-	local aDamage = {};
-	local aDamageNodes = UtilityManager.getSortedTable(DB.getChildren(nodeWeapon, "damagelist"));
-	for _,v in ipairs(aDamageNodes) do
-		local nMod = DB.getValue(v, "bonus", 0);
-		local sAbility = DB.getValue(v, "stat", "");
-		if sAbility == "base" then
-			sAbility = sBaseAbility;
-		end
-		if sAbility ~= "" then
-			local nAbilityBonus = ActorManager2.getAbilityBonus(rActor, sAbility);
-			local nMult = DB.getValue(v, "statmult", 1);
-			if nAbilityBonus > 0 and nMult ~= 1 then
-				nAbilityBonus = math.floor(nMult * nAbilityBonus);
-			end
-			nMod = nMod + nAbilityBonus;
-		end
-		
-		local aDice = DB.getValue(v, "dice", {});
-		if #aDice > 0 or nMod ~= 0 then
-			local sDamage = StringManager.convertDiceToString(DB.getValue(v, "dice", {}), nMod);
-			local sType = DB.getValue(v, "type", "");
-			if sType ~= "" then
-				sDamage = sDamage .. " " .. sType;
-			end
-			table.insert(aDamage, sDamage);
-		end
-	end
-
-	damageview.setValue(table.concat(aDamage, "\n"));
+	ActionDamage.performRoll(draginfo, rActor, rAction);
+	return true;
 end
 
