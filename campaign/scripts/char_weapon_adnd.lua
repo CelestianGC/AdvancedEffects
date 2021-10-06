1a38ac2eb8c760226f5b096df0f8421382e99387
Wil Thieme <wil.thieme@protonmail.com>
2021-10-06 07:45:28 -0400

    improve general compatibility with all extensions

--- campaign/scripts/char_weapon_adnd.lua
+++ campaign/scripts/char_weapon_adnd.lua
@@ -3,68 +3,6 @@
 -- attribution and copyright information.
 --
 
-function onInit()
-	local nodeWeapon = getDatabaseNode();
-	local nodeChar = DB.getChild(nodeWeapon, "...");
-	DB.addHandler(nodeWeapon.getNodeName(), "onChildUpdate", onDataChanged);
-	DB.addHandler(DB.getPath(nodeChar, "abilities.*.score"), "onUpdate", onDataChanged);
-	DB.addHandler(DB.getPath(nodeChar, "weapon.twoweaponfighting"), "onUpdate", onDataChanged);
-
-	onDataChanged();
-end
-
-function onClose()
-	local nodeWeapon = getDatabaseNode();
-	local nodeChar = DB.getChild(nodeWeapon, "...");
-	DB.removeHandler(nodeWeapon.getNodeName(), "onChildUpdate", onDataChanged);
-	DB.removeHandler(DB.getPath(nodeChar, "abilities.*.score"), "onUpdate", onDataChanged);
-	DB.removeHandler(DB.getPath(nodeChar, "weapon.twoweaponfighting"), "onUpdate", onDataChanged);
-end
-
-local m_sClass = "";
-local m_sRecord = "";
-function onLinkChanged()
-	local node = getDatabaseNode();
-	local sClass, sRecord = DB.getValue(node, "shortcut", "", "");
-	if sClass ~= m_sClass or sRecord ~= m_sRecord then
-		m_sClass = sClass;
-		m_sRecord = sRecord;
-		
-		local sInvList = DB.getPath(DB.getChild(node, "..."), "inventorylist") .. ".";
-		if sRecord:sub(1, #sInvList) == sInvList then
-			carried.setLink(DB.findNode(DB.getPath(sRecord, "carried")));
-		end
-	end
-end
-
-function onDataChanged()
-	onLinkChanged();
-	onAttackChanged();
-	onDamageChanged();
-	
-	local bRanged = (type.getValue() ~= 0);
-	label_ammo.setVisible(bRanged);
-	maxammo.setVisible(bRanged);
-	ammocounter.setVisible(bRanged);
-end
-
-function highlightAttack(bOnControl)
-	if bOnControl then
-		attackshade.setFrame("rowshade");
-	else
-		attackshade.setFrame(nil);
-	end
-end
-	
-function onAttackChanged()
-	local nodeWeapon = getDatabaseNode();
-	local nodeChar = nodeWeapon.getChild("...")
-
-	local nMod = CharWeaponManager.getAttackBonus(nodeChar, nodeWeapon);
-	
-	attackview.setValue(nMod);
-end
-
 function onAttackAction(draginfo)
 	local nodeWeapon = getDatabaseNode();
 	local nodeChar = nodeWeapon.getChild("...")
@@ -104,15 +42,6 @@ function onAttackAction(draginfo)
 	-- end bmos only allowing attacks when ammo is sufficient
 end
 
-function onDamageChanged()
-	local nodeWeapon = getDatabaseNode();
-	local nodeChar = nodeWeapon.getChild("...")
-	
-	local sDamage = CharWeaponManager.buildDamageString(nodeChar, nodeWeapon);
-
-	damageview.setValue(sDamage);
-end
-
 function onDamageAction(draginfo)
 	local nodeWeapon = getDatabaseNode();
 	local nodeChar = nodeWeapon.getChild("...")
@@ -131,5 +60,4 @@ function onDamageAction(draginfo)
 	
 	ActionDamage.performRoll(draginfo, rActor, rAction);
 	return true;
-
 end
